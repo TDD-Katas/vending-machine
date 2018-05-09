9b2f4550016d3b7c9b7679b46f2b420b0197e484
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-05-09 21:38:35 +0100

    Setup Gradle with JUnit5 support

---
+++ src/main/java/com/vocalink/interview/VendingMachine.java
@@ -0,0 +1,8 @@
+package com.vocalink.interview;
+
+public class VendingMachine {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/vocalink/interview/VendingMachineTest.java
@@ -0,0 +1,15 @@
+package com.vocalink.interview;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class VendingMachineTest {
+
+    @Test
+    @DisplayName("ole")
+    void add() {
+        assertEquals(2, 1, "1 + 1 should equal 2");
+    }
+}
